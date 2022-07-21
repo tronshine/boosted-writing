1d54dd744add0ec15c881b1de1dfb673c1285b58
Gergo Miklos <gergo.marcell.miklos@gmail.com>
2022-07-21 13:33:38 +0200

    fix analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
     <Script id="google-analytics" strategy="afterInteractive">
       {`
         window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-        ga('create', '${process.env.GOOGLE_ANALYTICS_ID}', 'auto');
+        ga('create', 'G-2KC2ME7XX5', 'auto');
         ga('send', 'pageview');
       `}
     </Script>
@@ -28,7 +28,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
       {`
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:'${process.env.HOTJAR_SITE_ID}',hjsv:6};
+            h._hjSettings={hjid:'3076388',hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
