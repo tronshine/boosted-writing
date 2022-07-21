0b6a7e085dcb9055c5e58566e3777fcf41902c2d
Gergo Miklos <gergo.marcell.miklos@gmail.com>
2022-07-21 13:16:44 +0200

    introduce analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import Script from 'next/script';
 
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
@@ -10,6 +11,31 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
       <meta name='Boosted riting' content='Writing productivity boosting tool with paid time limits' />
       <link rel='icon' href='/favicon.ico' />
     </Head>
+
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+        ga('create', '${process.env.GOOGLE_ANALYTICS_ID}', 'auto');
+        ga('send', 'pageview');
+      `}
+    </Script>
+    <Script
+      src="https://www.google-analytics.com/analytics.js"
+      strategy="afterInteractive"
+    />
+
+    <Script id="hotjar-tracking" strategy="afterInteractive">
+      {`
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:${process.env.HOTJAR_SITE_ID},hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}
+    </Script>
 
     <div className='background min-h-screen'>
       <Component {...pageProps} />
